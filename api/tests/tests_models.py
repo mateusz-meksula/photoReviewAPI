965f454b1cdfee2e38a75c3a1351a5ee4771ee4e
MeksulaM <mateusz.meksula@gmail.com>
2023-04-18 21:20:16 +0200

    Review model and model tests

--- api/tests/tests_models.py
+++ api/tests/tests_models.py
@@ -5,7 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
-from ..models import Photo, Tag
+from ..models import Photo, Tag, Review
 
 User = get_user_model()
 
@@ -72,3 +72,56 @@ class TagTestCase(TestCase):
         self.assertRaises(ValidationError, t2.full_clean)
         self.assertRaises(ValidationError, t3.full_clean)
         self.assertRaises(ValidationError, t4.full_clean)
+
+
+class ReviewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.author = User.objects.create_user(
+            username="testUser",
+            email="testemail@test.com",
+            password="testpassword123",
+        )
+        self.u = User.objects.create_user(
+            username="testUser2",
+            email="testemail2@test.com",
+            password="test2password123",
+        )
+        self.image = SimpleUploadedFile(
+            name="test_image.jpg", content=b"", content_type="image/jpg"
+        )
+        self.photo = Photo.objects.create(
+            author=self.author,
+            image=self.image,
+            title="test title",
+            description="test description",
+        )
+        self.review = Review.objects.create(
+            author=self.u,
+            photo=self.photo,
+            rating=4,
+            body="test review",
+        )
+
+    def tearDown(self) -> None:
+        if os.path.isfile("media/photos/test_title.jpg"):
+            os.remove("media/photos/test_title.jpg")
+
+    def test_review_created(self):
+        self.assertEqual(Review.objects.count(), 1)
+        r = Review.objects.first()
+        self.assertEqual(r.author, self.u)
+        self.assertEqual(r.photo, self.photo)
+        self.assertEqual(r.rating, 4)
+        self.assertEqual(r.body, "test review")
+
+    def test_rating_validation(self):
+        r1 = Review(author=self.u, photo=self.photo, rating=0)
+        r2 = Review(author=self.u, photo=self.photo, rating=6)
+        r3 = Review(author=self.u, photo=self.photo, rating="abc")
+        r4 = Review(author=self.u, photo=self.photo, rating="0")
+        r5 = Review(author=self.u, photo=self.photo, rating="6")
+        self.assertRaises(ValidationError, r1.full_clean)
+        self.assertRaises(ValidationError, r2.full_clean)
+        self.assertRaises(ValidationError, r3.full_clean)
+        self.assertRaises(ValidationError, r4.full_clean)
+        self.assertRaises(ValidationError, r5.full_clean)
